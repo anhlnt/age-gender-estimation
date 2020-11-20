5249fa87d7214e6d49cd7928bd26f28b039d088c
tuananh2494 <tuananh.2494@gmail.com>
2020-11-20 19:04:03 +0900

    Changed resolution

--- demo.py
+++ demo.py
@@ -61,8 +61,8 @@ def video_capture(*args, **kwargs):
 def yield_images():
     # capture video
     with video_capture(0) as cap:
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
         while True:
             # get video frame
