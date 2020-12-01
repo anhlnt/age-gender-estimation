0a1c3a289a33c96c586ae8219911dbe51724f6d9
tuananh2494 <tuananh.2494@gmail.com>
2020-12-01 19:17:24 +0900

    Fix web viewer position

--- start_webview.py
+++ start_webview.py
@@ -1,3 +1,6 @@
 import webview
-webview.create_window('VTM AI', url='http://localhost')
+from demo import getScreenSize
+
+SCREENSIZE = getScreenSize()
+webview.create_window('VTM AI', url='http://localhost', x=SCREENSIZE[0] // 2, y=0, width=SCREENSIZE[0] // 2, height=SCREENSIZE[1])
 webview.start()
